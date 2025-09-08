88d8637bd75cd2210f0cd061ae07756e6378176f
Mahmoud Deghedy <mahmuddeghady@gmail.com>
2025-09-08 12:49:11 +0300

    Adding Interactive Dashboard

--- main.py
+++ main.py
@@ -1,28 +1,113 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
+import altair as alt 
 
-st.title("Hello, Streamlit!")   
+st.set_page_config(layout="wide")
+#
 
+df = pd.read_csv('Telco_customer_churn.csv')
 
-df = pd.DataFrame({
-    'Category': ['A', 'B', 'C'],
-    'Value': [10, 20, 30], 
-    'Product': ['Apple', 'Banana', 'Carrot']
-}) 
+st.sidebar.header('Customer Filters')
 
 
-option = st.selectbox('Select a category:', df['Category'])
-
-filtered_df = df[df['Category'] == option] 
-
-st.write(filtered_df)
-
-chart = alt.Chart(filtered_df).mark_bar().encode(
-    x='Product',
-    y='Value'
-).properties(
-    title='Product Value by Category'
+contract_filter = st.sidebar.multiselect(
+    'Select a Contract Type:',
+    options=df['Contract'].unique(),
+    default=df['Contract'].unique()
 )
 
-st.altair_chart(chart, use_container_width=True)
+
+
+internet_service_filter = st.sidebar.multiselect(
+    'Select an Internet Service Type:',
+    options=df['Internet Service'].unique(),
+    default=df['Internet Service'].unique()
+)
+
+tenure_filter = st.slider(
+    'Select Tenure Months:',
+    min_value=int(df['Tenure Months'].min()),
+    max_value=int(df['Tenure Months'].max()),
+    value=(int(df['Tenure Months'].min()), int(df['Tenure Months'].max()))
+)
+
+
+filtered_df = df[
+  (df['Contract'].isin(contract_filter)) &
+  (df['Internet Service'].isin(internet_service_filter)) &
+  (df['Tenure Months'] >= tenure_filter[0]) & (df['Tenure Months'] <= tenure_filter[1])
+]
+
+total_customers = filtered_df.shape[0] 
+
+# Calculating the Churn Rate per each Contract Type
+churn_rate_by_contract = filtered_df.groupby('Contract')['Churn Value'].mean().reset_index()
+
+churn_rate_by_contract_chart = alt.Chart(churn_rate_by_contract).mark_bar().encode(
+    y=alt.X('Contract:N', title=''),
+    x=alt.Y('Churn Value:Q', title='Churn Rate', axis=alt.Axis(format='%')),
+    color='Contract'
+).properties(
+    title='Churn Rate by Contract Type'
+)
+
+
+churn_rate_by_internet_service = filtered_df.groupby('Internet Service')['Churn Value'].mean().reset_index()
+churn_rate_by_internet_service_chart = alt.Chart(churn_rate_by_internet_service).mark_bar().encode(
+    y=alt.X('Internet Service:N', title=''),
+    x=alt.Y('Churn Value:Q', title='Churn Rate', axis=alt.Axis(format='%')),
+    color='Internet Service'
+).properties(
+    title='Churn Rate by Internet Service Type'
+)
+
+churn_reasons = filtered_df[filtered_df['Churn Reason'] != 'Don\'t know'].groupby('Churn Reason')['Count'].sum().sort_values(ascending=False).reset_index().head()
+
+
+tab1, tab2 = st.tabs(["Churn Overview", "Deep Dive"])
+
+
+with tab1:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.metric(
+            label="Total Customers",
+            value=filtered_df.shape[0],
+
+        )
+    with col2:
+         churn_rate = filtered_df['Churn Value'].mean()
+         st.metric(
+         label="Overall Churn Rate",
+         value=f"{churn_rate:.1%}" # Format as a percentage
+    )
+
+    st.altair_chart(churn_rate_by_contract_chart, use_container_width=True)
+
+    st.altair_chart(churn_rate_by_internet_service_chart, use_container_width=True)
+
+  
+with tab2:
+
+  
+  churn_reasons_count = alt.Chart(churn_reasons).mark_bar().encode( 
+      y=alt.X('Churn Reason', title='', sort='-x'),
+      x=alt.Y('Count', title=''),
+      color='Churn Reason'
+      ).properties(
+          title = 'Top 5 Churn Reasons'
+      )
+
+  
+  st.altair_chart(churn_reasons_count, use_container_width=True)
+
+
+
+
+
+
+
+
+
+
+
